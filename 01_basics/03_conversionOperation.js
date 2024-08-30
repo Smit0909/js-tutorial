a99f9c28172a4fa5d4b4536667aba92bfc2a0e0d
Smit0909 <112684715+Smit0909@users.noreply.github.com>
2024-08-30 17:45:59 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "70abc";
+
+console.log(typeof(score));
+
+let scoreInNumber = Number(score);
+console.log(typeof(scoreInNumber));
+console.log(scoreInNumber);
+
+/*
+"70" => 70
+"70abc" => NaN
+true => 1; false=>0; 
+*/
