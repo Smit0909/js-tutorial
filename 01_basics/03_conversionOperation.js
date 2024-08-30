3338edc47c6d1b5f846d41e7bc07f4089b931786
Smit0909 <112684715+Smit0909@users.noreply.github.com>
2024-08-30 18:04:12 +0000

    added operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,28 @@
 let score = "70abc";
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let scoreInNumber = Number(score);
-console.log(typeof(scoreInNumber));
-console.log(scoreInNumber);
+// console.log(typeof(scoreInNumber));
+// console.log(scoreInNumber);
 
 /*
 "70" => 70
 "70abc" => NaN
 true => 1; false=>0; 
 */
+
+// -------------------Operations----------------------------
+let str1 = "Hello";
+let str2 = " Guest";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+
