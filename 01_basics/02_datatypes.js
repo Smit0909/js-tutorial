653560bb37a44976b2d5f21312ff8bb1785c0bc0
Smit0909 <112684715+Smit0909@users.noreply.github.com>
2024-08-30 17:45:41 +0000

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Hello"); //we are using nodejs, not browser
+// code readability should be high
+
+/* datatypes:
+number => 2 to the power 53
+bigint
+string =>""
+boolean => true/false
+null => standalone value
+undefined => value not defined;
+symbol => unique
+
+object
+
+*/
+
+console.log(typeof null); //object => mostImp
