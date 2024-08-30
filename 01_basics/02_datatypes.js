2d72effdc821d1f447deb6443d8c5559414d4c3c
Smit0909 <112684715+Smit0909@users.noreply.github.com>
2024-08-30 18:43:40 +0000

    memory information added

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -22,10 +22,32 @@ Functions
 
 //js is dynamically typed language => that means no need to specify the datatype of variable at declaration.
 
-console.log(typeof null); //object => mostImp
+// console.log(typeof null); //object => mostImp
 
 const id = Symbol('123');
 const anotherId = Symbol('123');
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
+
+
+// Stack(Primitive), Heap(Non-Pritive);
+
+let myName = "smit";
+let anotherName = myName;  //copy of myName
+anotherName = "abc";
+
+// console.log(myName);
+// console.log(anotherName);
+
+let userOne = {
+    email: "abc@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne; //pointing to userOne not copy
+
+userOne.email = "xyz@gmail.com";
+console.log(userTwo.email);
+
+
 
