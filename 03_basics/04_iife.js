200eae43b266fa4e256692abaf392d92f534a76f
Smit0909 <112684715+Smit0909@users.noreply.github.com>
2024-09-01 12:35:08 +0000

    changes in iife file

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -9,3 +9,5 @@
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
+
+//iife helps us to prevent pollution of global variables. means if we have declared one variable inside iife using var method(ex. var temp = 5;) then it is not accessible outside of iife.
